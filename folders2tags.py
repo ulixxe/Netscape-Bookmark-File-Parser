fc0bb85bb64edb62423c2c61a190dc062ce424e6
ulixxe <1309337+ulixxe@users.noreply.github.com>
2021-09-04 13:17:38 +0200

    folders2tags cleanup

--- folders2tags.py
+++ folders2tags.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 from NetscapeBookmarkFileParser.parser import parse, NetscapeBookmark, NetscapeBookmarkFileData
 from NetscapeBookmarkFileParser.exporter import netscape_html
 
-def merge_tags(old: str, new: str):
+def merge_tags(old: str, new: str) -> str:
     merged_tags = {tag.strip().casefold(): tag.strip() for tag in old.split(',')}
     for tag in new.split(','):
         if tag.strip().casefold() not in merged_tags:
@@ -12,12 +12,12 @@ def merge_tags(old: str, new: str):
     merged_tags = ','.join(merged_tags.values())
     return merged_tags
 
-def collect_bookmarks(bookmarks_dict: dict, bookmarks_data: NetscapeBookmarkFileData, path: str = ''):
+def collect_bookmarks(bookmarks_dict: dict, bookmarks_data: NetscapeBookmarkFileData, path: str = '') -> dict:
     for bookmark in bookmarks_data.bookmarks:
+        bookmark.attributes['tags'] = merge_tags(bookmark.attributes.get('tags', ''), path.replace('/', ','))
         if bookmark.attributes['href'] in bookmarks_dict:
-            bookmarks_dict[bookmark.attributes['href']].attributes['tags'] = merge_tags(bookmarks_dict[bookmark.attributes['href']].attributes['tags'], merge_tags(bookmark.attributes.get('tags', ''), path.replace('/', ',')))
+            bookmarks_dict[bookmark.attributes['href']].attributes['tags'] = merge_tags(bookmarks_dict[bookmark.attributes['href']].attributes['tags'], bookmark.attributes['tags'])
         else:
-            bookmark.attributes['tags'] = merge_tags(bookmark.attributes.get('tags', ''), path.replace('/', ','))
             bookmarks_dict[bookmark.attributes['href']] = bookmark
     for sub_folder in bookmarks_data.sub_folders:
         bookmarks_dict = collect_bookmarks(bookmarks_dict, sub_folder, path+'/'+sub_folder.folder)
@@ -34,6 +34,3 @@ netscape_bookmarks = NetscapeBookmarkFileData(
 )
 
 print(netscape_html(netscape_bookmarks))
-
-
-
